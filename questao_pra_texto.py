a1fce516c7ad7ee2c835c2354439be7de49f4952
MatheusAquinodeSouza <105607037+MatheusAquinodeSouza@users.noreply.github.com>
2022-11-11 18:49:45 -0300

    Update questao_pra_texto.py

--- questao_pra_texto.py
+++ questao_pra_texto.py
@@ -1,1 +1,14 @@
+def questao_para_texto(dicionario,numero):
 
+    var_x = ''
+    tracos = '------------------------------------- '
+    inicio = 'QUESTAO' + ' ' + str(numero) + '\n\n'
+    d = dicionario['titulo'] + '\n\n'
+    e = 'RESPOSTAS:\n'
+
+    for letra, texto in dicionario['opcoes'].items():
+        var_x += (f'{letra}: {texto}' '\n')
+    
+    final = tracos + inicio + d + e + var_x
+
+    return final

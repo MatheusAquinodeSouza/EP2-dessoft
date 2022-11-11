30d0de1da047113fffce4f1702491a7132aa958c
MatheusAquinodeSouza <105607037+MatheusAquinodeSouza@users.noreply.github.com>
2022-11-11 18:56:32 -0300

    Update codigos.py

--- codigos.py
+++ codigos.py
@@ -192,3 +192,17 @@ def sorteia_questao_inedita(dic, nivel, lista):
     sort=random.choice(dic[nivel])
     lista.append(sort)
     return sort
+def questao_para_texto(dicionario,numero):
+
+    var_x = ''
+    tracos = '------------------------------------- '
+    inicio = 'QUESTAO' + ' ' + str(numero) + '\n\n'
+    d = dicionario['titulo'] + '\n\n'
+    e = 'RESPOSTAS:\n'
+
+    for letra, texto in dicionario['opcoes'].items():
+        var_x += (f'{letra}: {texto}' '\n')
+    
+    final = tracos + inicio + d + e + var_x
+
+    return final
